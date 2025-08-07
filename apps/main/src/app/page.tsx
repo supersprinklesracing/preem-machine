786685ec0016724dc0cd6967ade83893215e5d74
Joe LaPenna <jlapenna@gmail.com>
2025-08-07 14:01:28 -0700

    chore: Rename the default app "main"

--- apps/main/src/app/page.tsx
+++ apps/main/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome @preem-machine/preem-machine 👋
+              Welcome @preem-machine/main 👋
             </h1>
           </div>
 
@@ -395,7 +395,7 @@ export default function Index() {
                 </svg>
                 View project details
               </summary>
-              <pre>nx show project @preem-machine/preem-machine --web</pre>
+              <pre>nx show project @preem-machine/main --web</pre>
             </details>
             <details>
               <summary>
