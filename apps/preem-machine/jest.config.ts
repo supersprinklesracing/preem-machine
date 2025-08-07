ede67af8489bad4bd47bb38642a23ff4b400aef2
Joe LaPenna <jlapenna@gmail.com>
2025-08-07 11:05:57 -0700

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/uUP1pqxb2q

---
+++ apps/preem-machine/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@preem-machine/preem-machine',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/preem-machine',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
