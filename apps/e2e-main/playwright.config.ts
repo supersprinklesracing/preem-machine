d3f42dd042337c5ae8b9436bb585959f46f47d80
Joe LaPenna <jlapenna@gmail.com>
2025-08-07 14:01:28 -0700

    chore: Rename the default app "main"

--- apps/e2e-main/playwright.config.ts
+++ apps/e2e-main/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run @preem-machine/preem-machine:start',
+    command: 'npx nx run @preem-machine/main:start',
     url: 'http://localhost:3000',
     reuseExistingServer: true,
     cwd: workspaceRoot,
--- apps/main/jest.config.ts
+++ apps/main/jest.config.ts
@@ -6,13 +6,13 @@ const createJestConfig = nextJest({
 });
 
 const config: Config = {
-  displayName: '@preem-machine/preem-machine',
+  displayName: '@preem-machine/main',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/preem-machine',
+  coverageDirectory: '../../coverage/apps/main',
   testEnvironment: 'jsdom',
 };
 
