6f95f1bb1ffde9c4d2544a555eab1b18208e6ce6
jlapenna <jlapenna@gmail.com>
2025-08-17 23:07:19 +0000

    chore: Initial dev.nix config for Firebase Studio

---
+++ .idx/dev.nix
@@ -0,0 +1,36 @@
+{ pkgs }: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_22
+    pkgs.hostname-debian
+  ];
+  idx.extensions = [
+    "dbaeumer.vscode-eslint"
+    "esbenp.prettier-vscode"
+    "firsttris.vscode-jest-runner"
+    "github.vscode-github-actions"
+    "GitHub.vscode-pull-request-github"
+    "ms-playwright.playwright"
+    "nrwl.angular-console"
+    "rvest.vs-code-prettier-eslint"
+  ];
+  idx.workspace.onCreate = {
+    npm-install = "npm install";
+  };
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "nx"
+          "dev"
+          "apps/main"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
