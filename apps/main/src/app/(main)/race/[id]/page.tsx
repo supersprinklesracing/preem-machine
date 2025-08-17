a1ad76fc2849fdbd0f2bb0e33821fd4520fcb722
Joe LaPenna <jlapenna@gmail.com>
2025-08-17 00:12:13 -0700

    fix: Future proof pages by calling await on params.

--- apps/main/src/app/(main)/race/[id]/page.tsx
+++ apps/main/src/app/(main)/race/[id]/page.tsx
@@ -3,7 +3,7 @@ import { getRenderableRaceDataForPage } from '@/datastore/firestore';
 import { notFound } from 'next/navigation';
 
 export default async function RacePage({ params }: { params: { id: string } }) {
-  const data = await getRenderableRaceDataForPage(params.id);
+  const data = await getRenderableRaceDataForPage((await params).id);
 
   if (!data) {
     notFound();
