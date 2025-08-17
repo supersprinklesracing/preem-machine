5c73110401c89ae9034fbb38403d487bb6427ebf
Joe LaPenna <jlapenna@gmail.com>
2025-08-17 11:30:42 -0700

    test(e2e): Move example e2e test to ensure the login page works

---
+++ apps/e2e-main/src/login.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/login');
+
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+});
