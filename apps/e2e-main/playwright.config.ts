60874a7ad8e2e6eed5a261c894d75660101fc90a
Joe LaPenna <jlapenna@gmail.com>
2025-08-24 14:07:19 -0700

    chore: disable webkit playwright tests.

--- apps/e2e-main/playwright.config.ts
+++ apps/e2e-main/playwright.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, devices } from '@playwright/test';
-import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
+import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test';
 
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
@@ -40,10 +40,13 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
+    // Disable webkit while it is not possible to run on a local machine.
+    /*
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    */
 
     // Uncomment for mobile browsers support
     /* {
