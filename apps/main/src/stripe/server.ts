94d6abbfc00942fe51c0c05e77663d7f8faff3b9
Joe LaPenna <jlapenna@gmail.com>
2025-08-26 09:03:43 -0700

    fix: Remove unnecessary stripe console warning.

--- apps/main/src/stripe/server.ts
+++ apps/main/src/stripe/server.ts
@@ -1,9 +1,5 @@
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_API_KEY) {
-  console.warn('STRIPE_API_KEY environment variable not set.');
-}
-
 export const getStripeServer = () => {
   return new Stripe(process.env.STRIPE_API_KEY ?? '', {
     // @ts-expect-error We specify the version in the environment.
