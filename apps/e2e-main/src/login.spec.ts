acdcc8a090e23099e3d2714432cfc2b00a3465ac
Joe LaPenna <jlapenna@gmail.com>
2025-09-07 12:48:46 -0700

    fix: use import...type...firebase-admin/firestore

---
+++ apps/e2e-main/src/login.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('login', () => {
+  test('has title', async ({ page }) => {
+    await page.goto('/login');
+
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+  });
+});
