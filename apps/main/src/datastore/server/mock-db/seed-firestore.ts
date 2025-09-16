89696ec47c55fa9f5493676266c3f837094cadec
Joe LaPenna <jlapenna@gmail.com>
2025-09-16 11:58:41 -0700

    Fix seeding / clearing docs in firestore in e2e tests

--- apps/main/src/datastore/server/mock-db/seed-firestore.ts
+++ apps/main/src/datastore/server/mock-db/seed-firestore.ts
@@ -1,5 +1,4 @@
 import { getFirestore } from '@/firebase/server';
-import type { Firestore } from 'firebase-admin/firestore';
 import { createMockDb } from './mock-db-util';
 import { DatabaseDocument } from './mock-db-processor';
 
@@ -42,11 +41,47 @@ async function seedCollection(
 }
 
 /**
+ * Deletes all documents in a collection and all their subcollections.
+ */
+async function clearCollection(
+  db: FirebaseFirestore.Firestore,
+  collectionPath: string,
+) {
+  const collectionRef = db.collection(collectionPath);
+  const querySnapshot = await collectionRef.get();
+
+  if (querySnapshot.size === 0) {
+    return;
+  }
+
+  const batch = db.batch();
+  querySnapshot.docs.forEach((doc) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
+
+  console.log(`Deleted all documents in collection: ${collectionPath}`);
+}
+
+/**
+ * Clears the entire Firestore database.
+ */
+async function clearFirestore(db: FirebaseFirestore.Firestore) {
+  console.log('Clearing Firestore database...');
+  const collections = await db.listCollections();
+  for (const collection of collections) {
+    await clearCollection(db, collection.id);
+  }
+  console.log('OK! Firestore database cleared.');
+}
+
+/**
  * Seeds the entire Firestore database based on the MOCK_DB structure.
  */
-export async function seedFirestore(firestore: Firestore) {
+export async function seedFirestore() {
   try {
     const db = await getFirestore();
+    await clearFirestore(db);
     console.log('Starting Firestore database seeding...');
     const mockDb = createMockDb(db);
 
