24c466bb291a87786e3d2642be2452d352f37f29
Joe LaPenna <jlapenna@gmail.com>
2025-09-16 10:38:46 -0700

    fix: do not use e2e env vars in unittests; clear up authentication error logging.

--- apps/main/src/app/(main)/manage/preem/edit/page.test.tsx
+++ apps/main/src/app/(main)/manage/preem/edit/page.test.tsx
@@ -15,9 +15,9 @@ jest.mock('./edit-preem-action', () => ({
 setupMockDb();
 
 describe('EditPreemPage component', () => {
-  it.skip('should fetch preem data and render the EditPreem component', async () => {
+  it('should fetch preem data and render the EditPreem component', async () => {
     const searchParams = Promise.resolve({
-      path: 'organizations/org-super-sprinkles/series/series-sprinkles-2025/events/event-giro-sf-2025/races/race-giro-sf-2025-masters-women/preems/preem-1',
+      path: 'organizations/org-super-sprinkles/series/series-sprinkles-2025/events/event-giro-sf-2025/races/race-giro-sf-2025-masters-women/preems/preem-giro-sf-2025-masters-women-first-lap',
     });
     const PageComponent = await EditPreemPage({ searchParams });
     render(PageComponent);
@@ -25,12 +25,14 @@ describe('EditPreemPage component', () => {
     expect(screen.getByText('Mock EditPreem')).toBeInTheDocument();
 
     const editPreemCalls = (EditPreem as jest.Mock).mock.calls;
-    expect(editPreemCalls[0][0].preem.id).toBe('preem-1');
+    expect(editPreemCalls[0][0].preem.id).toBe(
+      'preem-giro-sf-2025-masters-women-first-lap',
+    );
   });
 
-  it.skip('should throw NotFoundError when the preem does not exist', async () => {
+  it('should throw NotFoundError when the preem does not exist', async () => {
     const searchParams = Promise.resolve({
-      path: 'organizations/org-super-sprinkles/series/series-sprinkles-2025/events/event-giro-sf-2025/races/race-giro-sf-2025-masters-women/preems/non-existent-preem',
+      path: 'organizations/org-super-sprinkles/series/series-sprinkles-2025/events/event-giro-sf-2025/races/race-giro-sf-2025-masters-women/preems/does-not-exist',
     });
     expect(EditPreemPage({ searchParams })).rejects.toThrow(NotFoundError);
   });
--- apps/main/src/datastore/errors.ts
+++ apps/main/src/datastore/errors.ts
@@ -1,8 +1,9 @@
+export class AuthorizationError extends Error {}
 export class InvalidPathError extends Error {}
 export class NotFoundError extends Error {}
 
-export function unauthorized(): never {
-  throw new Error('Unauthorized');
+export function unauthorized(message = 'Unauthorized'): never {
+  throw new AuthorizationError(message);
 }
 
 export function notFound(message?: string, cause?: Error): never {
