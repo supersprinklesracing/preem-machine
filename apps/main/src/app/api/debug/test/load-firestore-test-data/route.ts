ababd2279289b45cbd00eaa773448037d16ee015
Joe LaPenna <jlapenna@gmail.com>
2025-10-05 20:03:07 -0700

    temp: allow seeding firestore even in prod

--- apps/main/src/app/api/debug/test/load-firestore-test-data/route.ts
+++ apps/main/src/app/api/debug/test/load-firestore-test-data/route.ts
@@ -1,13 +1,8 @@
 import { NextResponse } from 'next/server';
 
 import { seedFirestore } from '@/datastore/server/mock-db/seed-firestore';
-import { ENV_E2E_TESTING } from '@/env/env';
 
 export async function POST(_request: Request) {
-  if (!ENV_E2E_TESTING) {
-    return new Response('Not found', { status: 404 });
-  }
-
   try {
     await seedFirestore();
     return NextResponse.json({ success: true });
