717a2d68f2a50be25790dd52fe97e607bcf02e33
Joe LaPenna <jlapenna@gmail.com>
2025-10-09 20:08:13 -0700

    Update reset-password.spec.ts

--- apps/e2e-main/src/reset-password.spec.ts
+++ apps/e2e-main/src/reset-password.spec.ts
@@ -12,7 +12,6 @@ test.describe('reset-password', () => {
   });
   test.describe('authed', () => {
     test('basic', async ({ page }) => {
-      import { expect } from './util/fixtures';
       await page.goto('/reset-password');
       await expect(
         page.getByRole('heading', { name: 'Reset Password' }),
