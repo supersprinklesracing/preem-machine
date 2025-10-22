3a52e99ba3e814bbbd57e52eb098fd0bb4a670bb
Joe LaPenna <jlapenna@gmail.com>
2025-10-21 19:00:02 -0700

    fix: Add uid to UserContextValue (#367)

--- apps/main/src/user/client/UserContext.ts
+++ apps/main/src/user/client/UserContext.ts
@@ -4,11 +4,13 @@ import { AuthUser } from '@/auth/user';
 import type { User } from '@/datastore/schema';
 
 export interface UserContextValue {
+  uid: string | null;
   authUser: AuthUser | null;
   user: User | null;
 }
 
 export const UserContext = createContext<UserContextValue>({
+  uid: null,
   authUser: null,
   user: null,
 });
