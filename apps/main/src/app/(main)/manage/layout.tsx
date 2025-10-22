a57624a183a2785ca19ce78477d037e8611750e7
Joe LaPenna <jlapenna@gmail.com>
2025-10-21 18:09:48 -0700

    fix: Properly redirect incomplete users when using the main app, to the new-user page (#365)

--- apps/main/src/app/(main)/manage/layout.tsx
+++ apps/main/src/app/(main)/manage/layout.tsx
@@ -2,7 +2,7 @@
 
 import { requireLoggedInUserContext } from '@/user/server/user';
 
-export default async function ManageLayout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
